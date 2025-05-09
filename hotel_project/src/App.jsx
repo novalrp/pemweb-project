afd5b52f1f9599684773f6177feff2b47fa3f52b
herdiansyahsubakti <herdiansyahsubakti@gmail.com>
2025-05-10 05:54:24 +0700

    menambahkan bagian footer section dsb

--- hotel_project/src/App.jsx
+++ hotel_project/src/App.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './pages/Home'
-import Allrooms from './pages/Allrooms'
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import AllRooms from "./pages/AllRooms";
+import RoomDetails from "./pages/RoomDetails";
 
 const App = () => {
 
-  const isOwnerpath = useLocation().pathname.includes("owner")
+const isOwnerPath = useLocation().pathname.includes("owner");
+
 
   return (
     <div>
-     {!isOwnerpath && <Navbar/>}
-     <div className='min-h-[70vh]'>
+      {!isOwnerPath && <Navbar />}
+    <div className='min-h-[70vh]'>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/rooms' element={<Allrooms/>} />
+        <Route path="/" element={<Home/>}/>
+        <Route path="/rooms" element={<AllRooms/>}/>
+        <Route path="/rooms/:id" element={<RoomDetails/>}/>
       </Routes>
-     </div>
-     <Footer />
     </div>
-  )
-}
+    <Footer/>
+</div>
+  );
+};
 
-export default App
+export default App;
